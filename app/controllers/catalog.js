6a94e979e6d52e4d37c2353f8174ac0fa97b2690
luizhp <luizhp@yahoo.com>
2022-08-19 16:03:24 -0300

    refactor: cleaning

--- app/controllers/catalog.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  app.get("/catalog", function (req, res) {
-    let view = 'html/catalog/html';
-    console.log(view);
-    res.render(view, {});
-  });
-}
--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,25 @@
 // $('body').click(function () {
 //   pincodeFocus();
 // });
+
+new gridjs.Grid({
+  columns: [
+    {
+      name: '#',
+      formatter: (_, row) => gridjs.html(`<center><a href="javascript:getSong(${row.cells[0].data}, showSong);">${row.cells[0].data}</a></center>`)
+      //formatter: (cell) => `<center>${cell}</center>`
+    },
+    "singer",
+    "title"
+  ],
+  pagination: {
+    limit: 8
+  },
+  search: true,
+  sort: true,
+  fixedHeader: true,
+  server: {
+    url: '/songs',
+    then: data => data.map(card => [card.id, card.singer, card.title])
+  }
+}).render(document.getElementById("wrapper"));
